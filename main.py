d40f6e85eb9bb817232c6f6bb0694d9530013dfd
ivholmlu <ivholmlu@nmbu.no>
2024-01-11 22:24:26 +0100

    Adding vanillalowrank and toml config to run

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
-config = toml.load("config.toml")
+
+config = toml.load("config_examples/vanillalowrank.toml")
 train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
 train_loader = DataLoader(train_dataset, batch_size=config["settings"]["batch_size"], shuffle=True)
 
 test_dataset = datasets.MNIST(root='./data', train=False, transform=transform)
 test_loader = DataLoader(test_dataset, batch_size=config["settings"]["batch_size"], shuffle=False)
 
-config = toml.load("config.toml")
 criterion = nn.CrossEntropyLoss()
 flatten = nn.Flatten()
 net = NeuralNetwork(config)
--- src/layers.py
+++ src/layers.py
@@ -26,3 +26,29 @@ class Denselayer(nn.Module):
         self._b.data = self._b - self.lr*self._b.grad
         self._W.grad.zero_()
         self._b.grad.zero_()
+
+class VanillaLowRank(nn.Module):
+    
+    def __init__(self, config, lr) -> None:
+        super(VanillaLowRank, self).__init__()
+
+        self._U = nn.Parameter(torch.randn(config["dim_in"], config["rank"]), requires_grad=True)
+        self._S = nn.Parameter(torch.randn(config["rank"], config["rank"]), requires_grad=True)
+        self._VT = nn.Parameter(torch.randn(config["rank"], config["dim_out"]), requires_grad=True)
+
+        self._b = nn.Parameter(torch.randn(config["dim_out"]), requires_grad=True)
+
+        activation = ActivationFactory()
+        self.activation = activation(config["activation"])
+        self.lr = lr
+
+    def forward(self, X):
+        return self.activation(torch.matmul(X, self._W) + self._b)
+    
+    def step(self):
+        self._U.data = self._U - self.lr*self._U.grad
+        self._S.data = self._S - self.lr*self._S.grad
+        self._U.data = self._VT - self.lr*self._VT.grad
+        self._b.data = self._b - self.lr*self._b.grad
+        self._W.grad.zero_()
+        self._b.grad.zero_()
