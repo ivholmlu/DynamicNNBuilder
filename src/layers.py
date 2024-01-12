742e300145b8e0623f97d3b30803998d0a3b6628
ivholmlu <ivholmlu@nmbu.no>
2024-01-12 15:30:39 +0100

    Adding check for update of coefficients in step methods

--- src/layers.py
+++ src/layers.py
@@ -34,11 +34,12 @@ class Denselayer(nn.Module):
         return self.activation(torch.matmul(X, self._W) + self._b)
     
     @torch.no_grad
-    def step(self):
-        self._W.data = self._W - self.lr*self._W.grad
-        self._b.data = self._b - self.lr*self._b.grad
-        self._W.grad.zero_()
-        self._b.grad.zero_()
+    def step(self, s):
+        if not s:
+            self._W.data = self._W - self.lr*self._W.grad
+            self._b.data = self._b - self.lr*self._b.grad
+            self._W.grad.zero_()
+            self._b.grad.zero_()
 
 class VanillaLowRank(nn.Module):
     
@@ -59,29 +60,57 @@ class VanillaLowRank(nn.Module):
         W = torch.matmul(torch.matmul(self._U, self._S), self._VT)
         return self.activation(torch.matmul(X, W) + self._b)
     
-    def step(self):
-        self._U.data = self._U - self.lr*self._U.grad
-        self._S.data = self._S - self.lr*self._S.grad
-        self._VT.data = self._VT - self.lr*self._VT.grad
-        self._b.data = self._b - self.lr*self._b.grad
-        self._U.grad.zero_()
-        self._S.grad.zero_()
-        self._VT.grad.zero_()
-        self._b.grad.zero_()
+    def step(self, s):
+        if not s:
+            self._U.data = self._U - self.lr*self._U.grad
+            self._S.data = self._S - self.lr*self._S.grad
+            self._VT.data = self._VT - self.lr*self._VT.grad
+            self._b.data = self._b - self.lr*self._b.grad
+            self._U.grad.zero_()
+            self._S.grad.zero_()
+            self._VT.grad.zero_()
+            self._b.grad.zero_()
 
 class LowRank(nn.Module):
     def __init__(self, config, lr) -> None:
         super(LowRank, self).__init__()
-        self._U = nn.Parameter(torch.randn(config["dim_in"], config["rank"]), requires_grad=True)
-        self._S = nn.Parameter(torch.randn(config["rank"], config["rank"]), requires_grad=True)
-        self._VT = nn.Parameter(torch.randn(config["rank"], config["dim_out"]), requires_grad=True)
-
+        self._r = config["rank"]
         self.lr = lr
         activation = ActivationFactory()
         self.activation = activation(config["activation"])
 
-    def forward(self, X):
-        pass
+        self._U = nn.Parameter(torch.randn(config["dim_in"], self._r), requires_grad=True)
+        self._S = nn.Parameter(torch.randn(self._r, self._r), requires_grad=True)
+        self._V = nn.Parameter(torch.randn(config["dim_out"], self._r), requires_grad=True)
 
-    def step(self):
-        pass
+        
+
+    def forward(self, X):
+        W = torch.matmul(torch.matmul(self._U, self._S), self._V.T)
+        return self.activation(torch.matmul(X, W) + self._b)
+
+    def step(self, s):
+        
+        if not s:
+            # UPDATING K
+            K = torch.matmul(self._U, self._S)
+            dK = torch.matmul(self._U.grad, self._S)
+            K = K - self.lr * dK
+
+            U_new, _ = torch.linalg.qr(K)
+            M = torch.matmul(U_new.T, self._U)
+
+            #Updating L
+            L = torch.matmul(self._V, self._S.T)
+            dL = torch.matmul(self._V.grad[:, :self._r], self._S.T)
+            L = L - self.lr * dL
+
+            V_new, _ = torch.linal.qr(L)
+            N = torch.matmul(V_new.T, self._V)
+        
+            #Define S
+            self.S_tilde = torch.matmul(torch.matmul(M, self._S), N)
+
+        #Update S_tilde (Done after calculating loss for rest of network)
+
+
--- src/trainer.py
+++ src/trainer.py
@@ -1,9 +1,11 @@
+import torch
+import toml
+import torch.nn as nn
+
 from src.network import NeuralNetwork
 from src.layers import LayerFactory
-import torch.nn as nn
-import toml
 from src.loader import Loader
-import torch
+
 
 class Trainer:
     """Object for training a neural network.
@@ -16,25 +18,36 @@ class Trainer:
         self._criterion = nn.CrossEntropyLoss() #TODO Create a criterion factory
         loader = Loader()
         self._trainloader, self._testloader = loader.load_dataset(self._config)
+
+    @torch.no_grad
+    def test(self):
+        total = 0
+        correct = 0
+        for images, labels in self._testloader:
+            outputs = self.net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
         
     def train(self, show_progress=True):
         for i in range(self._iterations):
             for step, (images, labels) in enumerate(self._trainloader):
                 out = self.net(images)
                 loss = self._criterion(out, labels)
+                ### Update loss for network
                 loss.backward()
                 self.net.step()
+
+                #Update S in lowrank if network contains.
+                if self.net._contains_lowrank:
+                    self.net.step(s=True)
                 if (step + 1) % 100 == 0:
                     print(f'Epoch [{i+1}/{self._iterations}], Step [{step+1}/{len(self._trainloader)}], Loss: {loss.item():.4f}')
 
-            total = 0
-            correct = 0
+            if show_progress:
+                self.test()
             #TODO Isse with this not running as intende. No 
-            with torch.no_grad():
-                for images, labels in self._testloader:
-                    outputs = self.net(images)
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
+            
+
             accuracy = correct / total
             print(f'Epoch [{i+1}/{self._iterations}], Validation Accuracy: {100 * accuracy:.2f}%')
