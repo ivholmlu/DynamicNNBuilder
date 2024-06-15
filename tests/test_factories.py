91b77101d4ad231dbdd7ab3dcad56960a084c9e0
ivholmlu <ivholmlu@nmbu.no>
2024-06-15 17:16:54 +0200

    updating tests and test configs

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -4,23 +4,44 @@ import torch.nn as nn
 
 config = toml.load("tests/conf_test/test_conf.toml")
 
+"""
+[[layer]]
+type = 'dense'
+dims = [784, 512]
+dim_in = 784
+dim_out = 512
+activation = 'relu'
+
+[[layer]]
+type = 'dense'
+dims = [512, 256]
+dim_in = 512
+dim_out = 256
+activation = 'relu'
+
+[[layer]]
+type = 'dense'
+dims = [256, 10]
+dim_in = 256
+dim_out = 10
+activation = 'linear'
+"""
+
 def test_creation_layerfactory():
     layerfactory = LayerFactory()
     assert layerfactory.classes == {"dense": Denselayer,
                                     "vanillalowrank": VanillaLowRank,
                                     "lowrank": LowRank}
-    
+
+
 def test_return_layerfactory():
     layerfactory = LayerFactory()
-    layer = layerfactory(config["layers"][0])
+    layer = layerfactory(config["layer"][0])
     assert isinstance(layer, Denselayer)
     
-def test_creation_activationfactory():
-    activationfactory = ActivationFactory()
-    assert activationfactory.activations == {"relu": nn.ReLU(),
-                                            "identity": nn.Identity()}
+
     
 def test_return_activationfactory():
     activationfactory = ActivationFactory()
-    activation = activationfactory(config["layers"][0]["activation"])
+    activation = activationfactory(config["layer"][0]["activation"])
     assert isinstance(activation, nn.ReLU)
--- tests/test_network.py
+++ tests/test_network.py
@@ -4,54 +4,69 @@ import torch.nn as nn
 from src.network.network import NeuralNetwork
 import pytest
 
-#Several different configurations with dense layers
+# Several different configurations with dense layers
 single_dense_layer = toml.load("tests/conf_test/test_network.toml")
 config = toml.load("tests/conf_test/test_conf.toml")
 test_network_relu = toml.load("tests/conf_test/test_network_relu.toml")
 test_dense = toml.load("tests/conf_test/test_dense.toml")
 
-@pytest.mark.parametrize("config", [config, test_network_relu, single_dense_layer, test_dense])
+
+@pytest.mark.parametrize(
+        "conf",
+        [config, test_network_relu, single_dense_layer, test_dense])
 def test_creations_b(conf):
     "Test if the bias vector dimensions is created correctly"
     obj = NeuralNetwork(conf)
     for i, _ in enumerate(obj._layers):
         assert obj._layers[i]._b.size() == (conf["layer"][i]["dim_out"],)
 
-@pytest.mark.parametrize("config", [config, test_network_relu, single_dense_layer, test_dense])
+
+@pytest.mark.parametrize(
+        "conf", [config, test_network_relu, single_dense_layer, test_dense])
 def test_creations_dense_W(conf):
     "Test if the weight matrix dimensions is created correctly"
     obj = NeuralNetwork(conf)
     for i, _ in enumerate(obj._layers):
-        assert obj._layers[i]._W.size() == (conf["layer"][i]["dim_in"], conf["layer"][i]["dim_out"])
+        layer_dim_in = conf["layer"][i]["dim_in"]
+        layer_dim_out = conf["layer"][i]["dim_out"]
+        expected_size = (layer_dim_in, layer_dim_out)
+        assert obj._layers[i]._W.size() == expected_size
+
 
 def test_forward():
     """Testing 3x3 dense layer with identity activation"""
     network = NeuralNetwork(single_dense_layer)
-    network._layers[0]._b = nn.Parameter(torch.tensor([1, 1, 1], dtype=torch.float32))
-    network._layers[0]._W = nn.Parameter(torch.tensor([[1, 2, 3],  
-                        [4, 5, 6], 
-                        [7, 8, 9]], dtype=torch.float32))
+    network._layers[0]._b = nn.Parameter(
+        torch.tensor([1, 1, 1], dtype=torch.float32))
+    network._layers[0]._W = nn.Parameter(torch.tensor(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float32))
     x = torch.tensor([1, 0, -1], dtype=torch.float32).unsqueeze(0)
     Z = network.forward(x)
     assert torch.equal(Z, torch.tensor([[-5, -5, -5]], dtype=torch.int32))
 
+
 single_dense_layer_relu = toml.load("tests/conf_test/test_network_relu.toml")
+
+
 def test_forward_with_relu():
     "Testing 3x3 dense layer with ReLU"
     network = NeuralNetwork(single_dense_layer_relu)
-    network._layers[0]._W = nn.Parameter(torch.tensor([[1, -1, 0],  
-                                                        [0, 1, -1], 
-                                                        [-1, 0, 1]], dtype=torch.float32))
-    network._layers[0]._b = nn.Parameter(torch.tensor([1, 0, -1], dtype=torch.float32))
+    network._layers[0]._W = nn.Parameter(torch.tensor(
+        [[1, -1, 0], [0, 1, -1], [-1, 0, 1]], dtype=torch.float32))
+    network._layers[0]._b = nn.Parameter(
+        torch.tensor([1, 0, -1], dtype=torch.float32))
     x = torch.tensor([1, 2, 3], dtype=torch.float32).unsqueeze(0)
     Z = network(x)
 
     # Expected output should have non-negative values due to ReLU
     # Change these values according to the expected result after applying ReLU
-    expected_output = torch.tensor([[0, 1, 0]], dtype=torch.int32)  # Update as necessary
+    expected_output = torch.tensor([[0, 1, 0]], dtype=torch.int32)
     assert torch.equal(Z, expected_output)
 
+
 several_layer = toml.load("tests/conf_test/several_layer.toml")
+
+
 def test_entire_network_creation():
     network = NeuralNetwork(several_layer)
-    assert len(network._layers) == 3
+    assert len(network._layers) == 4
